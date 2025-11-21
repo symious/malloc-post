c29b573a0c7670afb20cee666cb3c7c9061a8cf6
Frank Rosner <3427394+FRosner@users.noreply.github.com>
2025-11-21 13:21:20 +0100

    more flexible plotting

--- benchmark_analysis.py
+++ benchmark_analysis.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from collections import defaultdict
 
-grouped = defaultdict(list)
+benchmarks = []
 results_folder = 'results'
 
 for filename in os.listdir(results_folder):
@@ -23,42 +23,46 @@ for filename in os.listdir(results_folder):
                     **bm
                 }
                 # {'bm_name': 'MallocFree_Multithreaded', 'implementation': 'system', 'size': 8192, 'name': 'BM_MallocFree_Multithreaded/8192/threads:8', 'family_index': 0, 'per_family_instance_index': 15, 'run_name': 'BM_MallocFree_Multithreaded/8192/threads:8', 'run_type': 'iteration', 'repetitions': 1, 'repetition_index': 0, 'threads': 8, 'iterations': 5712, 'real_time': 137789.86525395754, 'cpu_time': 137266.98179271698, 'time_unit': 'ns', 'items_per_second': 7285073.124941815}
-                grouped[implementation + "_" + bm_name].append(enriched_benchmark)
+                benchmarks.append(enriched_benchmark)
 
-def plot_data(y_label):
-    for result_name, data in grouped.items():
-        # Organize data by library
-        grouped_data = defaultdict(list)
-        for entry in data:
-            if y_label in entry:
-                grouped_data[entry["size"]].append((entry["threads"], entry[y_label]))
+def plot_data(plot_title, plot_filter, series_grouping, x_label, y_label):
+    print(f"Plotting {plot_title}, grouping by {series_grouping}, with x={x_label}, y={y_label}")
+    filtered_benchmarks = [bench for bench in benchmarks if plot_filter(bench)]
+    if not filtered_benchmarks:
+        return
 
-        if not grouped_data:
-            continue
+    grouped_data = defaultdict(list)
+    for entry in filtered_benchmarks:
+        print(entry)
+        if y_label in entry:
+            grouped_data[entry[series_grouping]].append((entry[x_label], entry[y_label]))
 
-        plt.figure(figsize=(10,6))
-        for library, points in grouped_data.items():
-            # Sort points by size for a clean plot
-            points.sort(key=lambda x: x[0])
-            sizes, ips = zip(*points)
-            plt.plot(sizes, ips, marker='o', label=library)
+    if not grouped_data:
+        return
 
-        plt.title(f"Benchmark: {result_name}")
-        plt.xlabel("Threads")
-        plt.ylabel(y_label.replace("_", " ").title())
-        plt.xscale('log', base=2)
+    plt.figure(figsize=(10,6))
+    for library, points in grouped_data.items():
+        # Sort points by size for a clean plot
+        points.sort(key=lambda x: x[0])
+        sizes, ips = zip(*points)
+        plt.plot(sizes, ips, marker='o', label=library)
 
-        all_threads = [size for points in grouped_data.values() for size, _ in points]
-        min_size = min(all_threads)
-        max_size = max(all_threads)
-        min_exp = int(np.floor(np.log2(min_size)))
-        max_exp = int(np.ceil(np.log2(max_size)))
-        ticks = 2 ** np.arange(min_exp, max_exp + 1)
-        plt.xticks(ticks)
+    plt.title(plot_title.replace("_", " ").title())
+    plt.xlabel(x_label.title())
+    plt.ylabel(y_label.replace("_", " ").title())
+    plt.xscale('log', base=2)
 
-        plt.legend()
-        plt.grid(True, which="both", ls="--")
-        plt.tight_layout()
-        plt.savefig(f"plots/{result_name}_{y_label}_results.png", dpi=300)
+    all_grouped_values = [size for points in grouped_data.values() for size, _ in points]
+    min_size = min(all_grouped_values)
+    max_size = max(all_grouped_values)
+    min_exp = int(np.floor(np.log2(min_size)))
+    max_exp = int(np.ceil(np.log2(max_size)))
+    ticks = 2 ** np.arange(min_exp, max_exp + 1)
+    plt.xticks(ticks)
 
-plot_data("items_per_second")
+    plt.legend()
+    plt.grid(True, which="both", ls="--")
+    plt.tight_layout()
+    plt.savefig(f"plots/{plot_title}_{y_label}_results.png", dpi=300)
+
+plot_data("malloc(512)", lambda b: b["size"] == 512, "implementation", "threads", "items_per_second")
